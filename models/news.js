484b99d2dbe5170d65ce9f0851ce1cdcb8f4deea
Joseph Tomoyasu <JTomoyasu@gmail.com>
2018-03-29 10:27:30 -0700

    code merge before testing

--- models/news.js
+++
@@ -1,9 +0,0 @@
-var API_KEY="7fe6a86c133e4266a390283e6305691a";
-const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI(API_KEY);
-
-newsapi.v2.topHeadlines({
-    country: 'us'
-}).then(function(res){
-    console.log(JSON.stringify(res, null, 2));
-});
