9d7d88bad07901ffdaf9e8063a0791c18b2065cd
Joseph Tomoyasu <JTomoyasu@gmail.com>
2018-03-29 10:09:03 -0700

    More routes

--- models/user.js
+++ models/user.js
@@ -17,31 +17,7 @@ module.exports = function (sequelize, DataTypes) {
                 len: [1]
             }
         },
-        username: { type: DataTypes.STRING, unique: true, validate: { notNull: true, notEmpty } },
-        password: { type: DataTypes.STRING, validate: { notNull: true, notEpty: true } }
-    },
-        {
-            //does the check to make sure its the right password to database
-            classMethods: {
-                validPassword: function (password, passwd, done, user) {
-                    bcrypt.compare(password.passwd, function (err, isMatch) {
-                        if (err) {
-                            console.log(err)
-                        }
-                        if (isMatch) {
-                            return done(null, user)
-                        }
-                        else {
-                            return done(null, false)
-                        }
-                    });
-                }
-            }
-        },
-        {
-            dialect: 'mysql'
-        }
-    );
+    });
     User.associate = function (models) {
         User.hasMany(model.Post, {
             onDelete: "cascade"
@@ -50,18 +26,5 @@ module.exports = function (sequelize, DataTypes) {
             onDelete: "cascade"
         });
     };
-    //everytime a user is created, this hashes the password.
-    User.hook('beforeCreate', function (user, fn) {
-        var salt = bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
-            return salt
-        })
-        bcrypt.hash(user.password, salt, null, function (err, hash) {
-            if (err) {
-                return next(err);
-                user.password = hash;
-                return fn(null, user)
-            }
-        });
-    });
     return User
 };
---
+++ routes/thread.js
@@ -0,0 +1,46 @@
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+    app.get("/api/threads", function(req, res) {
+        // Here we add an "include" property to our options in our findAll query
+        // We set the value to an array of the models we want to include in a left outer join
+        // In this case, just db.Post
+        db.Thread.findAll({
+          include: [db.Post]
+        }).then(function(dbThread) {
+          res.json(dbThread);
+        });
+      });
+    
+      app.get("/api/threads/:id", function(req, res) {
+        // Here we add an "include" property to our options in our findOne query
+        // We set the value to an array of the models we want to include in a left outer join
+        // In this case, just db.Post
+        db.Thread.findOne({
+          where: {
+            id: req.params.id
+          },
+          include: [db.Post]
+        }).then(function(dbThread) {
+          res.json(dbThread);
+        });
+      });
+    
+      app.post("/api/threads", function(req, res) {
+        db.Thread.create(req.body).then(function(dbThread) {
+          res.json(dbThread);
+        });
+      });
+    
+      app.delete("/api/threads/:id", function(req, res) {
+        db.Thread.destroy({
+          where: {
+            id: req.params.id
+          }
+        }).then(function(dbThread) {
+          res.json(dbThread);
+        });
+      });
+};
