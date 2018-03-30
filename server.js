854afb62f46ef29ce20bab430593b9095d3bf9da
David Turner <Dturner753@yahoo.com>
2018-03-30 16:14:52 -0700

    format changes and adding current data

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // For Passport
- 
-app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 
@@ -31,22 +31,21 @@ app.engine('hbs', exphbs({
 }));
 app.set('view engine', '.hbs');
 
- 
-app.get("/", function(req, res) {
+
+app.get("/", function (req, res) {
 
     res.sendFile(path.join(__dirname, "public/home2.html"));
- 
-    res.sendFile(path.join(__dirname, "public/home2.html"));
- 
- 
- 
-app.listen(PORT, function(err) {
- 
-    if (!err)
-        console.log("Site is live");
-    else console.log(err)
- 
 });
+
+
+
+    app.listen(PORT, function (err) {
+
+        if (!err)
+            console.log("Site is live");
+        else console.log(err)
+
+    });
 
 //Models
 var models = require("./app/models");
@@ -58,13 +57,13 @@ var authRoute = require('./app/routes/auth.js')(app, passport);
 require('./app/config/passport/passport.js')(passport, models.user);
 
 //Sync Database
-models.sequelize.sync().then(function() {
- 
+models.sequelize.sync().then(function () {
+
     console.log('Nice! Database looks fine');
     console.log("Local Host running on port: " + PORT);
- 
-}).catch(function(err) {
- 
+
+}).catch(function (err) {
+
     console.log(err, "Something went wrong with the Database Update!")
- 
+
 });
