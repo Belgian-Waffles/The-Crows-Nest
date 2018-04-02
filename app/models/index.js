8bdc19d4093eb9e92b52c0108a4eeed8c15f5db2
David Turner <Dturner753@yahoo.com>
2018-04-02 15:11:13 -0700

    heroku update

--- app/models/index.js
+++ app/models/index.js
@@ -3,6 +3,12 @@ var path = require("path");
 var Sequelize = require("sequelize");
 var env = process.env.NODE_ENV || "development";
 var config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
+if (env == "production"){
+    var sequelize = new Sequelize(config.use_env_variable);
+}
+else {
+    var sequelize = new Sequelize(config.database, config.username)
+}
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db = {};
  
